738308bedb93a7d86c3474c957acfd957c176752
PrathmeshFermion <prathm120814@gmail.com>
2024-02-15 12:35:25 +0530

    Right side navbar UI has been done now moving towards home route

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -31,7 +31,7 @@ function Theme() {
             className="active-theme"
           />
         </MenubarTrigger>
-        <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
+        <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border bg-white py-2 dark:border-dark-400 dark:bg-dark-300">
           {themes.map((item) => (
             <MenubarItem
               key={item.value}
