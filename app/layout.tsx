9f2fb1727a57d6ccbaa08ddcee2953dc5db42910
PrathmeshFermion <prathm120814@gmail.com>
2024-02-19 17:06:51 +0530

    vercel speed insight library added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { DESCRIPTION } from "@/constants/constant";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import ThemeProvider from "@/context/ThemeProvider";
@@ -39,6 +40,7 @@ export default function RootLayout({
             },
           }}>
           <ThemeProvider>{children}</ThemeProvider>
+          <SpeedInsights />
         </ClerkProvider>
       </body>
     </html>
